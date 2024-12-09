3d48a422afe47b66152f6019ba874c1262350e0b
Kyle Pace <kylepace03@gmail.com>
2024-12-08 21:06:42 -0500

    DaySeven Part Two.

--- DaySeven/DaySeven.cs
+++ DaySeven/DaySeven.cs
@@ -13,29 +13,42 @@ public class BridgeRepair
         input.Split(Environment.NewLine)
             .Select(line => line.Split(" ").Skip(1).Select(r => long.Parse(r)).Prepend(long.Parse(line.Split(":")[0])));
 
-    public static IEnumerable<long> FindPossibilities(long num, long[] vals)
+    public static IEnumerable<long> FindPossibilities(long num, long[] vals, char[] ops)
     {
         if (vals.Length == 0)
         {
             return [num];
         }
-
-        return FindPossibilities(num + vals.First(), vals.Skip(1).ToArray())
-            .Concat(FindPossibilities(num * vals.First(), vals.Skip(1).ToArray()));
+        var toReturn = new List<long>();
+        foreach (var op in ops)
+        {
+            switch (op) {
+                case '+':
+                    toReturn.AddRange(FindPossibilities(num + vals.First(), vals.Skip(1).ToArray(), ops));
+                    break;
+                case '*':
+                    toReturn.AddRange(FindPossibilities(num * vals.First(), vals.Skip(1).ToArray(), ops));
+                    break;
+                case '|':
+                    toReturn.AddRange(FindPossibilities(long.Parse(num.ToString() + vals.First().ToString()), vals.Skip(1).ToArray(), ops));
+                    break;
+            }
+        }
+        return toReturn;
     }
 
-    public static bool HasValidSolution(IEnumerable<long> input)
+    public static bool HasValidSolution(IEnumerable<long> input, char[] ops)
     {
         var values = input.Skip(1);
         var expected = input.ElementAt(0);
 
-        var possibilities = FindPossibilities(values.ElementAt(0), values.Skip(1).ToArray());
+        var possibilities = FindPossibilities(values.ElementAt(0), values.Skip(1).ToArray(), ops);
 
         return possibilities.Any(p => p == expected);
     }
 
-    public static long SumResults(IEnumerable<IEnumerable<long>> inputs) =>
-        inputs.Where(HasValidSolution).Sum(vs => vs.ElementAt(0));
+    public static long SumResults(IEnumerable<IEnumerable<long>> inputs, char[] ops) =>
+        inputs.Where(i => HasValidSolution(i, ops)).Sum(vs => vs.ElementAt(0));
 }
 
 public class DaySevenTests
@@ -57,7 +70,7 @@ public class DaySevenTests
 
         Assert.AreEqual(9, parsed.Count());
 
-        var sum = BridgeRepair.SumResults(parsed);
+        var sum = BridgeRepair.SumResults(parsed, ['+', '*']);
 
         Assert.AreEqual(3749, sum);
     }
@@ -69,7 +82,39 @@ public class DaySevenTests
         
         var parsed = BridgeRepair.ParseInput(fileInput);
 
-        var sum = BridgeRepair.SumResults(parsed);
+        var sum = BridgeRepair.SumResults(parsed, ['+', '*']);
+        
+        Assert.NotNull(sum);
+    }
+
+    [Test]
+    public void SecondExampleTest()
+    {
+        var puzzleInput = @"190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20";
+
+        var parsed = BridgeRepair.ParseInput(puzzleInput);
+
+        var sum = BridgeRepair.SumResults(parsed, ['+', '*', '|']);
+
+        Assert.AreEqual(11387, sum);
+    }
+
+    [Test, Ignore("slow - 3s")]
+    public async Task SecondInputTest()
+    {
+        var fileInput = await File.ReadAllTextAsync(@"DaySeven/DaySevenInput.txt");
+        
+        var parsed = BridgeRepair.ParseInput(fileInput);
+
+        var sum = BridgeRepair.SumResults(parsed, ['+', '*','|']);
         
         Assert.NotNull(sum);
     }
