d000c131536d7253a25d56554f49180643cb179b
Kyle Pace <kylepace03@gmail.com>
2024-12-10 21:37:50 -0500

    Day Ten part one.

---
+++ DayTen.cs
@@ -0,0 +1,98 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace AdventOfCode2024;
+
+public class DayTenTests
+{
+    public int[][] ParseTrail(string trailheads) =>
+        trailheads.Split(Environment.NewLine)
+            .Select(l => l.ToCharArray().Select(c => int.Parse(c.ToString())).ToArray()).ToArray();
+
+    public IList<(int, int)?> FindTrails(int[][] mtn, int x, int y)
+    {
+        var el = mtn[x][y];
+
+        if (el == 9)
+        {
+            return new List<(int, int)?> { (x, y) };
+        }
+
+        var acc = new List<(int, int)?>();
+        if (x - 1 >= 0 && mtn[x - 1][y] == el + 1)
+        {
+            acc.AddRange(FindTrails(mtn, x - 1, y));
+        }
+
+        if (x + 1 < mtn.Length && mtn[x + 1][y] == el + 1)
+        {
+            acc.AddRange(FindTrails(mtn, x + 1, y));
+        }
+
+        if (y - 1 >= 0 && mtn[x][y - 1] == el + 1)
+        {
+            acc.AddRange(FindTrails(mtn, x, y - 1));
+        }
+
+        if (y + 1 < mtn[x].Length && mtn[x][y + 1] == el + 1)
+        {
+            acc.AddRange(FindTrails(mtn, x, y + 1));
+        }
+
+        return acc;
+    }
+
+    public int ScoreTrail(int[][] mtn, int x, int y) =>
+        FindTrails(mtn, x, y).Where(i => i.HasValue).Distinct().Count();
+
+    public int ScoreTrails(string trailheads)
+    {
+        var mtn = ParseTrail(trailheads);
+
+        var trailScores = new List<int>();
+
+        for (var i = 0; i < mtn.Length; i++)
+        {
+            for (var j = 0; j < mtn[i].Length; j++)
+            {
+                if (mtn[i][j] == 0)
+                {
+                    trailScores.Add(ScoreTrail(mtn, i, j));
+                }
+            }
+        }
+    
+        return trailScores.Sum();
+    }
+
+    [Test]
+    public void ExampleTest()
+    {
+        var terrain = @"89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732";
+
+        var trailheadScore = ScoreTrails(terrain);
+
+        Assert.AreEqual(36, trailheadScore);
+    }
+
+    [Test]
+    public async Task InputTest()
+    {
+        var fileInput = await File.ReadAllTextAsync(@"Input/DayTenInput.txt");
+
+        var trailheadScore = ScoreTrails(fileInput);
+
+        Assert.NotNull(trailheadScore);
+    }
+}
